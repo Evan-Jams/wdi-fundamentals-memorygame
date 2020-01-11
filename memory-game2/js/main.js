8be724af0450733c19f613c32e0f5b1d427ba40e
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-11 10:39:49 -0500

    simplified main.js, added little extra

--- memory-game2/js/main.js
+++ memory-game2/js/main.js
@@ -1,16 +1,20 @@
 console.log("Up and running!");
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2 && 
-	cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
 }
+flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+		cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	checkForMatch();
+}
+}
+	
+
 
