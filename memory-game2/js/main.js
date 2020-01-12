e4b32938e8222c6f5502ed5d9a4065f370be4add
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-11 19:29:09 -0500

    deleted html card structures, added extra js

--- memory-game2/js/main.js
+++ memory-game2/js/main.js
@@ -1,20 +1,47 @@
 console.log("Up and running!");
-const cards = ["queen", "queen", "king", "king"];
+
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 const cardsInPlay = [];
+
 checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
+
 flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-		cardsInPlay.push(cards[cardId]);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+		cardsInPlay.push(cards[cardId].rank);
+if (cardsInPlay.length === 2) {
 	checkForMatch();
 }
 }
-	
-
-
+createBoard = function() {
+	for (let i = 0; i < cards.length; i++) {
+	}
